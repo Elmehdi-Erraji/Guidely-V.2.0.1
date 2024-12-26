f335804c18b11cd23443f300a9562dcfaab3d58d
Mehdi <elmehdiworkemail@gmail.com>
2024-12-26 07:25:04 +0000

    first commit

---
+++ src/main/java/com/spring/guidely/GuidelyApplication.java
@@ -0,0 +1,13 @@
+package com.spring.guidely;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuidelyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuidelyApplication.class, args);
+    }
+
+}
